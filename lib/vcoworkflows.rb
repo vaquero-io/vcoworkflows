7a3b3d2a4291c8677d879d1c2e19f94ccad22ecf
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-03 17:45:45 -0600

    Add Thor-based CLI ability

--- lib/vcoworkflows.rb
+++ lib/vcoworkflows.rb
@@ -1,5 +1,20 @@
 require "vcoworkflows/version"
+require 'thor'
 
 module Vcoworkflows
   # Your code goes here...
+
+  class CLI < Thor
+
+    package_name 'vcoworkflows'
+    map '--version' => :version
+    map '-v' => :version
+
+    desc 'version', DESC_VERSION
+    def version
+      puts VERSION
+    end
+
+  end
+
 end
