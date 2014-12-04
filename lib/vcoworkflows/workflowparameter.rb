43b8739033263012ea598d349cec1e81434b1406
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-04 17:48:44 -0600

    Handle array parameters properly

--- lib/vcoworkflows/workflowparameter.rb
+++ lib/vcoworkflows/workflowparameter.rb
@@ -4,23 +4,50 @@ require 'json'
 module VcoWorkflows
   class WorkflowParameter
 
-    attr_accessor :name
-    attr_accessor :type
+    attr_reader :name
+    attr_reader :type
+    attr_reader :subtype
     attr_accessor :required
     attr_accessor :value
 
     def initialize(name: nil, type: nil, required: false, value: nil)
-      @name = name unless name.nil?
-      @type = type unless type.nil?
+      @name = name
+
+      # set the type properly
+      case type
+      when /\//
+        @type = type.gsub(/\/.*$/, '')
+        @subtype = type.gsub(/^.*\//, '')
+      else
+        @type = type
+        @subtype = nil
+      end
+
       @required = required
-      @value = value
+
+      # If value is supposed to be an array but we dont' have a value yet,
+      # create an empty array
+      if @type.eql?('Array') && value.nil?
+        @value = []
+      else
+        @value = value
+      end
     end
 
     def as_struct
-      attributes = {:type => @type,
-                    :name => @name,
-                    :scope => 'local',
-                    :value => {@type => {:value => @value}}}
+      attributes = {:type => @type, :name => @name, :scope => 'local'}
+
+      # If the value is an array, we need to build it in the somewhat silly
+      # manner that vCO requires it to be presented. Otherwise, just paste
+      # it on the end of the hash.
+      if @type.eql?('Array')
+        fail(IOError, ERR[:wtf]) unless @value.is_a?(Array)
+        attributes[:value] = {@type => {:elements => []}}
+        @value.each { |val| attributes[:value][@type][:elements] << {@subtype => {:value => val}} }
+      else
+        attributes[:value] = {@type => {:value => @value}}
+      end
+
       return attributes
     end
 
