5aa88192d945acb7273d3ab01e690069b19a8575
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-04 17:48:21 -0600

    Add some more error messages

--- lib/vcoworkflows/constants.rb
+++ lib/vcoworkflows/constants.rb
@@ -6,10 +6,12 @@ module VcoWorkflows
 
   # error messages
   ERR = {
+      wtf: 'vcoworkflows: I have no idea what just went wrong.',
       no_workflow_found: 'vcoworkflows: no workflow found!',
       too_many_workflows: 'vcoworkflows: more than one workflow found for given name!',
       wrong_workflow_wtf: 'vcoworkflows: search returned the wrong workflow! (?)',
-      no_workflow_service_defined: 'vcoworkflows: Attempted to execute a workflow with a nil workflow service!'
+      no_workflow_service_defined: 'vcoworkflows: Attempted to execute a workflow with a nil workflow service!',
+      param_verify_failed: 'vcoworkflows: Attempt to verify required parameter failed!'
   }
 
 
