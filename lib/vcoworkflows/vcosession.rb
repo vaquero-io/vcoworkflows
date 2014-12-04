ddb266c14ab199e754908be0e4124bed5a548501
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-04 13:54:31 -0600

    Refactor VcoSession for RestClient

--- lib/vcoworkflows/vcosession.rb
+++ lib/vcoworkflows/vcosession.rb
@@ -1,40 +1,23 @@
 require_relative 'constants'
-require 'httparty'
-require 'erb'
-
-# Bring HTTP to the party
-include HTTParty
-
-include ERB::Util
+require 'rest_client'
 
 module VcoWorkflows
 
   class VcoSession
 
-    # Add headers to our requests
-    headers 'Content-Type' => 'application/json'
-    headers 'Accept' => 'application/json'
-
-    def initialize(uri: nil, user: nil, passwd: nil, sslverify: false)
-      # Set the httparty base_uri
-      @base_uri = "#{uri}/#{API_URL_BASE}"
-      # Configure the authentication
-      @auth = {:username => user, :password => passwd}
-      # Set SSL Verification
-      default_options.update(verify: sslverify)
+    def initialize(uri, user: nil, password: nil, verify_ssl: false)
+      api_url = "#{uri}/vco/api"
+      @rest_resource = RestClient::Resource.new(api_url, :user => user, :password => password, :verify_ssl => verify_ssl)
     end
 
-    def get(endpoint: nil)
-      options[:basic_auth] = @auth
-      response_json = self.class.get(@base_uri << url_encode(endpoint), options)
-      return response_json
+    def get(endpoint)
+      response = @rest_resource[endpoint].get :accept => :json
+      return response.body
     end
 
-    def post(endpoint: nil, body: nil)
-      options[:basic_auth] = @auth
-      options[:body] = body
-      response_json = self.class.post(@base_uri << url_encode(endpoint), options)
-      return response_json
+    def post(endpoint, body)
+      response = @rest_resource[endpoint].post body, :content_type => :json
+      return response.body
     end
 
   end
