8f87ec8eb71685f668dd7d55ab52c49d0a3cf771
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-04 13:55:12 -0600

    Start to flesh out WorkflowToken

--- lib/vcoworkflows/workflowtoken.rb
+++ lib/vcoworkflows/workflowtoken.rb
@@ -1,8 +1,22 @@
+require_relative 'constants'
+require_relative 'workflow'
+require_relative 'workflowservice'
+
 module VcoWorkflows
 
   class WorkflowToken
 
-    def initialize
+    attr_reader :id
+    attr_reader :title
+    attr_reader :workflow_id
+    attr_reader :current_item_name
+    attr_reader :current_item_state
+    attr_reader :global_state
+    attr_reader :start_date
+    attr_reader :end_date
+    attr_reader :xml_content
+
+    def initialize(token_json)
 
     end
 
