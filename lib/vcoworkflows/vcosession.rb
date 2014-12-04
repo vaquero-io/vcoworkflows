3b3191ffd085a59b9dc51d3c3f1a559207cc08a3
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-03 18:47:04 -0600

    try to get httparty to work

--- lib/vcoworkflows/vcosession.rb
+++ lib/vcoworkflows/vcosession.rb
@@ -1,12 +1,15 @@
 require_relative 'constants'
 require 'httparty'
+require 'erb'
+
+# Bring HTTP to the party
+include HTTParty
+
+include ERB::Util
 
 module VcoWorkflows
 
   class VcoSession
-
-    # Bring HTTP to the party
-    include 'httparty'
 
     # Add headers to our requests
     headers 'Content-Type' => 'application/json'
@@ -14,7 +17,7 @@ module VcoWorkflows
 
     def initialize(uri: nil, user: nil, passwd: nil, sslverify: false)
       # Set the httparty base_uri
-      base_uri "#{uri}/#{API_URL_BASE}"
+      @base_uri = "#{uri}/#{API_URL_BASE}"
       # Configure the authentication
       @auth = {:username => user, :password => passwd}
       # Set SSL Verification
@@ -23,14 +26,14 @@ module VcoWorkflows
 
     def get(endpoint: nil)
       options[:basic_auth] = @auth
-      response_json = self.class.get(endpoint, options)
+      response_json = self.class.get(@base_uri << url_encode(endpoint), options)
       return response_json
     end
 
     def post(endpoint: nil, body: nil)
       options[:basic_auth] = @auth
       options[:body] = body
-      response_json = self.class.post(endpoint, options)
+      response_json = self.class.post(@base_uri << url_encode(endpoint), options)
       return response_json
     end
 
