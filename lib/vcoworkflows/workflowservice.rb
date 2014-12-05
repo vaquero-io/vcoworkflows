a9b95b2ae436560907ced992b93df7c6e298e8da
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-05 16:54:17 -0600

    Handle checking up on running execution

--- lib/vcoworkflows/workflowservice.rb
+++ lib/vcoworkflows/workflowservice.rb
@@ -1,6 +1,7 @@
 require_relative 'constants'
 require_relative 'vcosession'
 require_relative 'workflow'
+require_relative 'workflowexecutionlog'
 require 'json'
 require 'erb'
 
@@ -29,7 +30,7 @@ module VcoWorkflows
     # @param [VcoWorkflows::Workflow] workflow - workflow GUID who's presentation we want
     # @return [VcoWorkflows::WorkflowPresentation]
     def get_presentation(workflow)
-      VcoWorkflows::WorkflowPresentation.new(@session.get("/workflows/#{workflow.id}/presentation/"), workflow)
+      VcoWorkflows::WorkflowPresentation.new(@session.get("/workflows/#{workflow.id}/presentation/").body, workflow)
     end
 
     # Public
@@ -37,7 +38,7 @@ module VcoWorkflows
     # @param [String] name - Name of the workflow
     # @return [VcoWorkflows::Workflow] - the requested workflow
     def get_workflow_for_name(name)
-      response = JSON.parse(@session.get("/workflows?conditions=name=#{url_encode(name)}"))
+      response = JSON.parse(@session.get("/workflows?conditions=name=#{url_encode(name)}").body)
 
       # barf if we got anything other than a single workflow
       fail(IOError, ERR[:too_many_workflows]) if response['total'] > 1
@@ -45,18 +46,22 @@ module VcoWorkflows
 
       # yank out the workflow id and name from the result attributes
       workflow_id = nil
-      workflow_name = nil
       response['link'][0]['attributes'].each do |a|
         workflow_id = a['value'] if a['name'].eql?('id')
-        workflow_name = a['value'] if a['name'].eql?('name')
       end
-
-      # Barf if we got this far and the workflow we found has a different
-      # name than what we went looking for.
-      # fail(IOError, ERR[:wrong_workflow_wtf]) unless workflow_name.eql?(name)
 
       # Get the workflow by GUID
       get_workflow_for_id(workflow_id)
+    end
+
+    def get_execution(workflow_id, execution_id)
+      response = @session.get("/workflows/#{workflow_id}/executions/#{execution_id}")
+      VcoWorkflows::WorkflowToken.new(response.body, workflow_id)
+    end
+
+    def get_log(workflow_id, execution_id)
+      response = @session.get("/workflows/#{workflow_id}/executions/#{execution_id}/logs/")
+      VcoWorkflows::WorkflowExecutionLog.new(response.body)
     end
 
     # Public
@@ -64,7 +69,10 @@ module VcoWorkflows
     # @param [String] id - Workflow GUID for the workflow we want to execute
     # @param [String] parameter_json - Required workflow input parameters as JSON
     def execute_workflow(id, parameter_json)
-      response = JSON.parse(@session.post("/workflows/#{id}/executions/", parameter_json))
+      # response = JSON.parse(@session.post("/workflows/#{id}/executions/", parameter_json))
+      response = @session.post("/workflows/#{id}/executions/", parameter_json)
+      execution_id = response.headers[:location].gsub(/^.*\/executions\//,'')
+      get_execution(id, execution_id)
     end
 
   end
--- lib/vcoworkflows/workflowtoken.rb
+++ lib/vcoworkflows/workflowtoken.rb
@@ -6,17 +6,46 @@ module VcoWorkflows
 
   class WorkflowToken
 
+    # "id": "ff80808148eb0494014a1c6314703d1d"
+    # "state": "running"
+    # "name": "Request Component"
+    # "href": "https://meorcpoc0001.dev.activenetwork.com:8281/vco/api/workflows/6e04a460-4a45-4e16-9603-db2922c24462/executions/ff80808148eb0494014a1c6314703d1d/"
+    # "start-date": 1417815463023
+    # "started-by": "gruiz-ade@DEV.ACTIVENETWORK.COM"
+    # "current-item-display-name": "__item-undefined__"
+
     attr_reader :id
-    attr_reader :title
     attr_reader :workflow_id
-    attr_reader :current_item_name
-    attr_reader :current_item_state
-    attr_reader :global_state
+    attr_reader :name
+    attr_reader :state
+    attr_reader :href
     attr_reader :start_date
     attr_reader :end_date
-    attr_reader :xml_content
+    attr_reader :started_by
+    attr_reader :current_item_name
+    attr_reader :current_item_state
+    attr_reader :content_exception
+    attr_reader :global_state
+    attr_reader :json_content
 
-    def initialize(token_json)
+    def initialize(token_json, workflow_id)
+
+      @json_content = token_json
+      @workflow_id = workflow_id
+
+      token = JSON.parse(token_json)
+
+      @id                 = token.key?('id')                        ? token['id']                        : nil
+      @name               = token.key?('name')                      ? token['name']                      : nil
+      @state              = token.key?('state')                     ? token['state']                     : nil
+      @href               = token.key?('href')                      ? token['href']                      : nil
+      @start_date         = token.key?('start-date')                ? token['start-date']                : nil
+      @end_date           = token.key?('end-date')                  ? token['end-date']                  : nil
+      @started_by         = token.key?('started-by')                ? token['started-by']                : nil
+      @current_item_name  = token.key?('current-item-display-name') ? token['current-item-display-name'] : nil
+      @current_item_state = token.key?('current-item-state')        ? token['current-item-state']        : nil
+      @global_state       = token.key?('global-state')              ? token['global-state']              : nil
+      @content_exception  = token.key?('content-exeption')          ? token['content-exception']         : nil
 
     end
 
