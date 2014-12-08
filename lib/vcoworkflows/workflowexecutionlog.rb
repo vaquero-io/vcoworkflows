e77a0492975ed11a52d8ef5043238edeed3aab66
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-08 12:51:21 -0800

    Fix parsing and add to_s

--- lib/vcoworkflows/workflowexecutionlog.rb
+++ lib/vcoworkflows/workflowexecutionlog.rb
@@ -12,9 +12,22 @@ module VcoWorkflows
     # @param [String] log_json - JSON document as string
     def initialize(log_json)
       @messages = {}
-      JSON.parse(log_json).each do |log|
-        @messages[log['entry']['time-stamp']] = log['entry']
+      JSON.parse(log_json)['logs'].each do |log_entry|
+        messages[log_entry['entry']['time-stamp']] = log_entry['entry']
       end
+    end
+
+    # Public
+    # @return [String]
+    def to_s
+      message = "Workflow execution log:\n"
+      @messages.keys.sort.each do |timestamp|
+        message << "#{Time.at(timestamp/1000)}"
+        message << " #{@messages[timestamp]['severity']} #{@messages[timestamp]['user']}"
+        message << " #{@messages[timestamp]['short-description']};"
+        message << " #{@messages[timestamp]['long-description']}\n"
+      end
+      return message
     end
 
   end
