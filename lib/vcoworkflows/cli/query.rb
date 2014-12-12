b78e0426daadfd06692e2dcb6d951a04384f0d81
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2014-12-11 20:10:21 -0800

    Add a bunch of query capability

--- lib/vcoworkflows/cli/query.rb
+++ lib/vcoworkflows/cli/query.rb
@@ -17,6 +17,14 @@ module VcoWorkflows
       class_option :verify_ssl, type: :boolean, default: true, desc: DESC_CLI_VERIFY_SSL
       class_option :dry_run, type: :boolean, default: false, desc: DESC_CLI_DRY_RUN
 
+      class_option :executions, type: :boolean, aliases: '-e', default: false
+      class_option :executions_limit, type: :numeric, aliases: '-l', default: 0
+      class_option :execution_id, type: :string, aliases: '-I'
+      class_option :state, type: :boolean, aliases: '-r'
+      class_option :logs, type: :boolean, aliases: ['-L','--log']
+      class_option :input_parameters, type: :boolean, default: false
+      class_option :show_json, type: :boolean, default: false
+
       def self.source_root
         File.dirname(__FILE__)
       end
@@ -51,7 +59,51 @@ module VcoWorkflows
         end
 
         puts ""
-        puts wf
+        if options[:execution_id]
+          puts "Fetching data for execution #{options[:execution_id]}..."
+          execution = wfs.get_execution(wf.id, options[:execution_id])
+          if options[:state]
+            puts "Execution started at #{Time.at(execution.start_date/1000)}"
+            puts "Execution #{execution.state} at #{Time.at(execution.end_date/1000)}"
+          else
+            puts ""
+            if options[:show_json]
+              puts execution.to_json
+            else
+              puts execution
+            end
+            if options[:input_parameters]
+
+            end
+          end
+
+          if options[:logs]
+            puts ""
+            wftoken = wfs.get_log(wf.id, options[:execution_id])
+            if options[:show_json]
+              puts wftoken.to_json
+            else
+              puts wftoken
+            end
+          end
+        else
+          puts wf
+        end
+        
+        if options[:executions]
+          puts "\nExecutions: "
+          executions = {}
+          wfs.get_execution_list(wf.id).each_value do |attrs|
+            executions[attrs['startDate']] = attrs
+          end
+          keys = executions.keys.sort
+          if options[:executions_limit] > 0
+            keys = keys.slice(keys.size - options[:executions_limit], keys.size)
+          end
+          keys.each do |timestamp|
+            puts "#{timestamp} [#{executions[timestamp]['id']}] #{executions[timestamp]['state']}"
+          end
+        end
 
       end
 
