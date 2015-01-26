29267e6f471a2943865fd7299f2044b3c01e791f
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2015-01-26 14:33:08 -0800

    Added accessor to aid unit testing

--- spec/vcoworkflows/vcosession_spec.rb
+++ spec/vcoworkflows/vcosession_spec.rb
@@ -4,25 +4,26 @@ require 'vcoworkflows'
 describe VcoWorkflows::VcoSession, 'VcoSession' do
 
   before(:each) do
-    @url = 'https://vcoserver.example.com:8281/'
+    @uri = 'https://vcoserver.example.com:8281'
     @username = 'johndoe'
     @password = 's3cr3t'
   end
 
   it "should set the URL" do
-    vs = VcoWorkflows::VcoSession.new(@url, user: @username, password: @password)
-    expect(vs.url).to eql(@url)
+    vs = VcoWorkflows::VcoSession.new(@uri, user: @username, password: @password)
+    api_url = '/vco/api'
+    expect(vs.rest_resource.url).to eql(@uri << api_url)
   end
 
 
   it "should set the username" do
-    vs = VcoWorkflows::VcoSession.new(@url, user: @username, password: @password)
-    expect(vs.user).to eql(@username)
+    vs = VcoWorkflows::VcoSession.new(@uri, user: @username, password: @password)
+    expect(vs.rest_resource.user).to eql(@username)
   end
 
   it "should set the password" do
-    vs = VcoWorkflows::VcoSession.new(@url, user: @username, password: @password)
-    expect(vs.password).to eql(@password)
+    vs = VcoWorkflows::VcoSession.new(@uri, user: @username, password: @password)
+    expect(vs.rest_resource.password).to eql(@password)
   end
 
 
