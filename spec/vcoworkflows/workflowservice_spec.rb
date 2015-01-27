155a3d4c16f865a6356a521a1e871beaebd3a330
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2015-01-26 18:47:59 -0800

    typofix

--- spec/vcoworkflows/workflowservice_spec.rb
+++ spec/vcoworkflows/workflowservice_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../spec_helper.rb'
 require 'vcoworkflows'
 
-describe VcoWorkflows::WorkflowService, 'WorkflowService', do
+describe VcoWorkflows::WorkflowService, 'WorkflowService' do
 
 end
